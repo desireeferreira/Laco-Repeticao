8738ed4eb4032ca7d82455f17936a79c3a2fa77b
Desiree Ferreira <desireeferreira6@gmail.com>
2025-02-05 22:27:15 -0300

    Descrição das alterações

--- src/AtividadePratica/atividadetreswhile.java
+++ src/AtividadePratica/atividadetreswhile.java
@@ -6,8 +6,31 @@ public class atividadetreswhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Scanner leia = new(System.in);
+        Scanner entrada = new Scanner (System.in);
         
+        int  menoresDe21=0;
+        int maioresDe50=0;
+        
+        System.out.println("Digite uma idade:");
+        int idade = entrada.nextInt();
+        
+        while (idade>=0) {
+        	if (idade <21) {
+        		menoresDe21++;
+        	}
+        	else if(idade > 50) {
+        		maioresDe50++;
+        	}
+        	
+        	System.out.println("Digite uma idade");
+        	idade= entrada.nextInt();
+        	}
+        System.out.println("Total de pessoas menores de 21 anos: " +menoresDe21);
+        System.out.println("Total de pessoas maiores de 50 anos: " + maioresDe50);
+        
+        entrada.close();
 	}
+	
+	
 
 }
