26e73fb1f6b326a575261419e1d1ad40e8c6cef5
Desiree Ferreira <desireeferreira6@gmail.com>
2025-02-05 22:52:06 -0300

    Atualização da atividade Do while

--- src/AtividadePratica/atividadecincodowhile.java
+++ src/AtividadePratica/atividadecincodowhile.java
@@ -1,10 +1,27 @@
 package AtividadePratica;
 
+import java.util.Scanner;
 public class atividadecincodowhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int soma= 0;
+		int numero;
+		Scanner leitor = new Scanner(System.in);
+		
+	do {
+		System.out.print("Digite um número: ");
+	     numero= leitor.nextInt();
+	
+	     if (numero >0) {
+	    	 soma+=numero;
+	     }
+	     } while(numero != 0);
+	
+	System.out.print("Os numeros positivos é:"+ soma );
+	
+	leitor.close();
 	}
 
 }
